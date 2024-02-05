7d7f2823195968c3be6b10af69315492e71a89a7
Jimmy Givans <jauyoma.dev@gmail.com>
2024-02-05 16:01:47 +0300

    feat: use express-fileupload to save files to upload directory

--- src/server/main.js
+++ src/server/main.js
@@ -4,20 +4,35 @@ import path from "node:path";
 import express from "express";
 import ViteExpress from "vite-express";
 import logger from "morgan";
+import fileUpload from "express-fileupload";
 
 const app = express();
 const PORT = 8080;
+const publicDir = path.join(path.dirname(fileURLToPath(import.meta.url)), "../../public");
+const uploadsDir = path.join(publicDir, "uploads");
 
 app.use(logger("short"));
+app.use(express.static(publicDir));
+
+app.use(fileUpload());
 
 app.get("/", (request, response) => {
-    response.sendFile("index.html", {
-        root: path.join(path.dirname(fileURLToPath(import.meta.url)), "../../")
-    });
+    response.sendFile(path.join(publicDir, "../index.html"));
 });
 
 app.post("/upload", (request, response) => {
-    return response.send("File uploaded");
+    if (!request.files || Object.keys(request.files).length === 0) {
+        return response.status(400).send("No files were uploaded");
+    }
+
+    const uploadedFile = request.files.file;
+    uploadedFile.mv(path.join(uploadsDir, uploadedFile.name), (err) => {
+        if (err) {
+            return response.status(500).send(err);
+        }
+
+        response.json({ success: true, message: "File uploaded successfully!" });
+    });
 });
 
 ViteExpress.listen(app, PORT, () => {
