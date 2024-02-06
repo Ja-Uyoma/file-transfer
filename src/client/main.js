2756fe64d35ae2b17ac7a42b9a880a4afb4c5073
Jimmy Givans <jauyoma.dev@gmail.com>
2024-02-06 15:22:59 +0300

    refactor: auto-discover the dropzone

--- src/client/main.js
+++ src/client/main.js
@@ -1,31 +1,27 @@
 import Dropzone from "dropzone";
 
-Dropzone.autoDiscover = false;
+const myDropzone = new Dropzone("#my-dropzone", {
+    url: "http://localhost:8080/upload",
+    paramName: "file",
+    maxFilesize: 2 * 1024,
+    addRemoveLinks: true,
+    dictDefaultMessage: "Upload"
+});
 
-document.addEventListener("DOMContentLoaded", () => {
-    const myDropzone = new Dropzone("#my-dropzone", {
-        url: "http://localhost:8080/upload",
-        paramName: "file",
-        maxFilesize: 2 * 1024,
-        addRemoveLinks: true,
-        dictDefaultMessage: "Upload"
-    });
-    
-    myDropzone.on("addedfile", (file) => {
-        console.log(`${file.name} added to the queue...`);
-    });
+myDropzone.on("addedfile", (file) => {
+    console.log(`${file.name} added to the queue...`);
+});
 
-    myDropzone.on("sending", (file) => {
-        console.log(`Sending file ${file.name}...`);
-    });
+myDropzone.on("sending", (file) => {
+    console.log(`Sending file ${file.name}...`);
+});
 
-    myDropzone.on("success", (file, response) => {
-        console.log(`${response.message}`);
-    });
+myDropzone.on("success", (file, response) => {
+    console.log(`${response.message}`);
+});
 
-    myDropzone.on("error", (file, response) => {
-        console.log("Could not upload file.\n\n");
-        console.log(file);
-        console.log(response);
-    });
+myDropzone.on("error", (file, response) => {
+    console.log("Could not upload file.\n\n");
+    console.log(file);
+    console.log(response);
 });
